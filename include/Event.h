40f70ef2b8ccf57a118c14efa2104131c09341dd
Brock York <twunkown@gmail.com>
2014-01-03 13:21:14 +0800

    Event.h 	Changed Event_net_type from a typedef enum to enum because it was wrong 	Changed Net_Event to now include all the information needed for a Net_Event update

--- include/Event.h
+++ include/Event.h
@@ -1,18 +1,77 @@
 #ifndef EVENT_H
 #define EVENT_H
 
+#include <string>
+#include <uuid/uuid.h>
+#include "Player.h"
+
 
 class Event
 {
     public:
-        typedef enum
+        enum Event_Net_type
         {
-            CONNECT = 0,
-            CONNECTED
-        }Event_type;
+            EVENT_NET_UNKNOWN = 0,
+            EVENT_NET_CONNECT,
+            EVENT_NET_CONNECT_ACK,
+            EVENT_NET_CONNECTED,
+            EVENT_NET_CONNECTED_ACK,
+            EVENT_NET_CONNECTING,
+            EVENT_NET_DISCONNECT,
+            EVENT_NET_DISCONNECTED
+        };
+
+        enum Event_type
+        {
+            EVENT_UNKNOWN = 0,
+            EVENT_NET,
+            EVENT_KEY,
+            EVENT_GAMEUPDATE
+        };
+
+        enum Event_Key_type
+        {
+            EVENT_KEY_DOWN = 0,
+            EVENT_KEY_UP
+        };
+
+        typedef struct
+        {
+            Event_Net_type net_type;
+            std::string address;
+            std::string port;
+        } Net_Event;
+
+        enum Key_Sym
+        {
+            KEY_A = 'a',
+            KEY_W = 'w',
+            KEY_S = 's',
+            KEY_D = 'd'
+        };
+
+        typedef struct
+        {
+            Event_Key_type key_type;
+            Key_Sym sym;
+
+        } Key_Event;
+
+        typedef struct
+        {
+            int x;
+            int y;
+            char character;
+        } GameUpdate_Event;
 
         Event_type type;
+        Net_Event net;
+        Key_Event key;
+        GameUpdate_Event update;
+        int id;
+
         Event();
+        //void operator=(Event event);
         virtual ~Event();
     protected:
     private:
