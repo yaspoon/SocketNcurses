e7c561709a9ddc582a262de0dd3a8838d0be5cfc
Brock York <twunkown@gmail.com>
2014-01-03 12:36:53 +0800

    Server modified all logging calls to include const_cast<char *>() to stop the compiler complaining about deprecated conversions

--- src/Server.cpp
+++ src/Server.cpp
@@ -6,27 +6,20 @@
 
 Server::Server()
 {
-
     SV_Address = "127.0.0.1";
     SV_Port = "34567";
-    uuid_generate(SV_ID);
-    clientCount = 0;
 }
 
 Server::Server(std::string address, std::string port)
 {
     SV_Address = address;
     SV_Port = port;
-    uuid_generate(SV_ID);
-    clientCount = 0;
 }
 
 Server::Server(std::string port)
 {
     SV_Address = "127.0.0.1";
     SV_Port = port;
-    uuid_generate(SV_ID);
-    clientCount = 0;
 }
 
 Server::~Server()
@@ -36,37 +29,115 @@ Server::~Server()
 
 returnCodes_t Server::run()
 {
-    net = Network(SV_Address, SV_Port, MODE_SERVER);
-    net.Setup();
+    //net = Network(SV_Address, SV_Port, MODE_SERVER);
+    net.Setup(SV_Address, SV_Port, MODE_SERVER);
+
+    char data;
 
     while(true)
     {
 
+        Event tmp = net.getEvent();
+
+        switch(tmp.type)
+        {
+            case Event::EVENT_NET:
+                handleNetEvent(tmp);
+                break;
+            case Event::EVENT_KEY:
+                handleKeyEvent(tmp);
+                break;
+        }
+
+        sendUpdate();
     }
 }
 
-bool Server::addClient(uuid_t id, struct sockaddr_storage peer, socklen_t peer_len, int initialFrameNum)
+bool Server::addClient(std::string address, std::string port, int id)
 {
     bool retVal = false;
 
-    if((clientCount + 1) <= 32)
+    if((clients.size() + 1) <= 32)
     {
         SV_Client newCLient;
-        newCLient.connected = true;
-        newCLient.frameNo = initialFrameNum;
+        newCLient.status = Event::EVENT_NET_CONNECTED;
+        newCLient.frameNo = 0;
         newCLient.nextFrameNo = 0;
         newCLient.ackNo = 0;
-        memcpy(&(newCLient.CL_addr), &peer, sizeof(peer));
-        newCLient.CL_addr_len = peer_len;
-        mempcpy(newCLient.id, id, sizeof(id));
+        newCLient.id = id;
+        log(LG_DEBUG, const_cast<char *>("Server::addClient Created Client[%d]"), id);
 
-        clients[clientCount] = newCLient;
-        clientCount += 1;
+        clients[id] = newCLient;
 
         retVal = true;
     }
 
     return retVal;;
+}
+
+void Server::handleNetEvent(Event event)
+{
+    Event::Net_Event net = event.net;
+    switch(net.net_type)
+    {
+        case Event::EVENT_NET_CONNECT:
+            log(LG_DEBUG, const_cast<char *>("Server::run got EVENT_CONNECTION from %s:%s"), net.address.c_str(), net.port.c_str());
+            addClient(net.address, net.port, event.id);
+            Event conAck;
+            conAck.type = Event::EVENT_NET;
+            conAck.net.net_type = Event::EVENT_NET_CONNECT_ACK;
+            conAck.net.address = net.address;
+            conAck.net.port = net.port;
+            conAck.id = event.id;
+            this->net.sendEvent(conAck);
+        break;
+    }
+}
+
+void Server::handleKeyEvent(Event event)
+{
+    switch(event.key.key_type)
+    {
+        case Event::EVENT_KEY_DOWN:
+        {
+            switch(event.key.sym)
+            {
+                case Event::KEY_A:
+                    clients[event.id].CL_player.x = clients[event.id].CL_player.x - 1;
+                    log(LG_DEBUG, const_cast<char *>("Client[%d].CL_Player.x=%d"), event.id, clients[event.id].CL_player.x);
+                break;
+                case Event::KEY_W:
+                    clients[event.id].CL_player.y = clients[event.id].CL_player.y - 1;
+                    log(LG_DEBUG, const_cast<char *>("Client[%d].CL_Player.y=%d"), event.id, clients[event.id].CL_player.y);
+                break;
+                case Event::KEY_S:
+                    clients[event.id].CL_player.y = clients[event.id].CL_player.y + 1;
+                    log(LG_DEBUG, const_cast<char *>("Client[%d].CL_Player.y=%d"), event.id, clients[event.id].CL_player.y);
+                break;
+                case Event::KEY_D:
+                    clients[event.id].CL_player.x = clients[event.id].CL_player.x + 1;
+                    log(LG_DEBUG, const_cast<char *>("Client[%d].CL_Player.x=%d"), event.id, clients[event.id].CL_player.x);
+                break;
+            }
+        }
+        break;
+    }
+}
+
+void Server::sendUpdate()
+{
+    Event update;
+    update.type = Event::EVENT_GAMEUPDATE;
+
+    for(int i = 0; i < clients.size(); i++)
+    {
+        //log(LG_DEBUG, "Sending update to Client[%d].id:%d c:%c x:%d y:%d", i, clients[i].id, clients[i].CL_player.character, clients[i].CL_player.x, clients[i].CL_player.y);
+        update.id = clients[i].id;
+        update.update.x = clients[i].CL_player.x;
+        update.update.y = clients[i].CL_player.y;
+        update.update.character = clients[i].CL_player.character;
+        net.sendEvent(update);
+    }
 }
 
 
