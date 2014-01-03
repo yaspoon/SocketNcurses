13ca03c8dece16aa3b6b1dd9d65e0388b407a81b
Brock York <twunkown@gmail.com>
2014-01-03 13:24:12 +0800

    Event.cpp 	Set default type in constructor to EVENT_UNKNOWN and set id to -1 	Added assignment operator overaloading but it's commented out 	because it was suppose to fix the linked list but it doesn't

--- src/Event.cpp
+++ src/Event.cpp
@@ -1,11 +1,41 @@
 #include "Event.h"
+#include "Common.h"
 
 Event::Event()
 {
-    //ctor
+    type = EVENT_UNKNOWN;
+    id = -1;
 }
 
 Event::~Event()
 {
     //dtor
 }
+
+/*void Event::operator=(Event event)
+{
+    //log(LG_DEBUG, "Event::operator=");
+    this->type = event.type;
+    this->id = event.id;
+
+    switch(event.type)
+    {
+        case EVENT_NET:
+        {
+            this->net = event.net;
+        }
+        break;
+        case EVENT_KEY:
+        {
+            this->key = event.key;
+        }
+        break;
+        case EVENT_GAMEUPDATE:
+        {
+            this->update.character = event.update.character;
+            this->update.x = event.update.x;
+            this->update.y = event.update.y;
+        }
+        break;
+    }
+}*/
