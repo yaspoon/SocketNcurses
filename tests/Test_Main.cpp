2a3296ff1f8f7f14bb4140f6743d80e130c83d66
Brock York <twunknown@gmail.com>
2017-05-15 20:55:51 +1000

    Add all the TestSuites and run them!

--- tests/Test_Main.cpp
+++ tests/Test_Main.cpp
@@ -1,10 +1,17 @@
 #include <cpptest.h>
 #include "Test_Buffer.h"
+#include "Test_BitsRequired.h"
+#include "Test_Frame.h"
+#include "Test_ReadWriteBuffer.h"
 
 int main(int argc, char *argv[])
 {
 	Test::TextOutput output(Test::TextOutput::Verbose);
 	BufferTestSuite bts;
-	return bts.run(output) ? EXIT_SUCCESS : EXIT_FAILURE;
+	BitsRequiredTestSuite brts;
+	FrameTestSuite fts;
+	ReadWriteBufferTestSuite rwbts;
+	return (bts.run(output) && brts.run(output) && rwbts.run(output) && fts.run(output)) ? EXIT_SUCCESS : EXIT_FAILURE;
+	
 
 }
