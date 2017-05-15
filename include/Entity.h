bccfc7da4c1d4d83a179b06bf550398ee53dd6fa
Brock York <twunknown@gmail.com>
2017-05-15 20:38:00 +1000

    Delete a bunch of code so the game compiles after the massive changes

--- include/Entity.h
+++ include/Entity.h
@@ -1,44 +1,12 @@
 #ifndef ENTITY_H
 #define ENTITY_H
 #include "Common.h"
-
-typedef struct
-{
-    int x;
-    int y;
-    char character;
-}Ent_player;
-
-union Ent_data
-{
-    Ent_player player;
-};
-
-enum Ent_type
-{
-    ENT_UNKNOWN = 0,
-    ENT_PLAYER
-};
-
-typedef struct
-{
-    Ent_type ent_type;
-    Ent_data data;
-
-} Ent;
-
+#include "Position2D.h"
 
 class Entity
 {
     public:
         Entity();
-        virtual Ent toEnt()
-        {
-            log(LG_DEBUG, const_cast<char *>("Entity::toEnt"));
-            Ent tmp;
-            tmp.ent_type = ENT_UNKNOWN;
-            return tmp;
-        }
         virtual ~Entity();
     protected:
     private:
