02eb5fd234b47fdfcb8da1efec85ac22b9f1f059
Brock York <twunknown@gmail.com>
2017-05-27 12:37:59 +1000

    Initial implementation of Game class
    Add serialisation function
    Move code from Server into Game as the responsibility
    for game logic has moved

--- include/Game.h
+++ include/Game.h
@@ -1,15 +1,22 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include "Event.h"
+#include "GameState.h"
+
 /*
  * Arbiter of all the game logic and data
  */
 class Game
 {
 private:
+	GameState state;
+	void handleKeyEvent(Event event);
+	bool addPlayer();
 public:
 	Game();
 	~Game();
+	template <typename Stream> bool serialise(Stream &stream);
 };
 
 #endif
--- src/Game.cpp
+++ src/Game.cpp
@@ -1,4 +1,10 @@
 #include "Game.h"
+#include <iostream>
+
+#include "WriteBuffer.h"
+#include "ReadBuffer.h"
+
+#include "Log.h"
 
 Game::Game()
 {
@@ -7,3 +13,66 @@ Game::Game()
 Game::~Game()
 {
 }
+
+bool Game::addPlayer()
+{
+	STUB << "Game::addPlayer" << STUB_END;
+	bool retVal = false;
+
+	return retVal;
+}
+
+void Game::handleKeyEvent(Event event)
+{
+    switch(event.key.key_type)
+    {
+        case Event::EVENT_KEY_DOWN:
+        {
+            switch(event.key.sym)
+            {
+                case Event::KEY_A:
+                {
+                    Player thePlayer = state.getPlayer(event.id);
+                    thePlayer.x -= 1;
+                    state.setPlayer(event.id, thePlayer);
+                    DEBUG << "Client[" << event.id << "].CL_Player.x=" << thePlayer.x << DEBUG_END;
+                }
+                break;
+                case Event::KEY_W:
+                {
+                    Player thePlayer = state.getPlayer(event.id);
+                    thePlayer.y -= 1;
+                    state.setPlayer(event.id, thePlayer);
+                    DEBUG << "Client[" << event.id << "].CL_Player.y=" << thePlayer.y << DEBUG_END;
+                }
+                break;
+                case Event::KEY_S:
+                {
+                    Player thePlayer = state.getPlayer(event.id);
+                    thePlayer.y += 1;
+                    state.setPlayer(event.id, thePlayer);
+                    DEBUG << "Client[" << event.id << "].CL_Player.y=" << thePlayer.y << DEBUG_END;
+                }
+                break;
+                case Event::KEY_D:
+                {
+                    Player thePlayer = state.getPlayer(event.id);
+                    thePlayer.x += 1;
+                    state.setPlayer(event.id, thePlayer);
+                    DEBUG << "Client[" << event.id << "].CL_Player.x=" << thePlayer.x << DEBUG_END;
+                }
+                break;
+            }
+        }
+        break;
+    }
+}
+
+template <typename Stream> bool Game::serialise(Stream &stream)
+{
+	STUB << "Game::serialise" << STUB_END;
+	return state.serialise(stream);
+}
+
+template bool Game::serialise<WriteBuffer>(WriteBuffer&);
+template bool Game::serialise<ReadBuffer>(ReadBuffer&);
