8277be459df3e61873e924cdd3d95dc1414fb501
Brock York <twunknown@gmail.com>
2017-05-30 14:37:00 +1000

    Add function to serialise buffers into a ReadBuffer

--- src/ReadBuffer.cpp
+++ src/ReadBuffer.cpp
@@ -36,3 +36,19 @@ bool ReadBuffer::SerialiseBits(uint64_t &input, uint32_t nobits)
 	input = Buffer::read(nobits);
 	return Buffer::getStatus() == OKAY;
 }
+
+bool ReadBuffer::SerialiseBuffer(Buffer &output)
+{
+	bool retval = true;
+	while(bitsLeft() > bitsPerWord && retval)
+	{
+		retval = output.writeUint64(readUint64());
+	}
+	uint32_t bits = bitsLeft();
+	if(retval == true)
+	{
+		output.write(read(bits), bits);
+	}
+
+	return retval;
+}
