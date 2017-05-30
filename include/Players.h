9478af1537630cd928e01d72249c606d789f1890
Brock York <twunknown@gmail.com>
2017-05-30 13:58:18 +1000

    Modify Player class to suite its new responsibility of Players
    Rename Player to players
    Change data structures to hold multiple players
    this is to make updating players faster and not
    wrech the caches on the processor

--- include/Players.h
+++ include/Players.h
@@ -1,23 +1,25 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
-#include "Entity.h"
+#include "Entitys.h"
 
+const uint32_t MAX_PLAYERS = 32;
 
-class Player : public Entity
+class Players : public Entitys
 {
     public:
-        Player();
-        virtual ~Player();
+        Players();
+        virtual ~Players();
 	template <typename Stream> bool serialise(Stream &stream);
-        int x;
-        int y;
-        char character;
 
         //Player& operator=(const Player& inPlayer);
         //Player& operator=(Player& inPlayer);
     protected:
     private:
+	uint32_t numPlayers;
+	Vec2D positions[MAX_PLAYERS];
+	Vec2D velocitys[MAX_PLAYERS];
+        char characters[MAX_PLAYERS];
 };
 
 #endif // PLAYER_H
